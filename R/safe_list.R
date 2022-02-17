0b8f191c77522341e405a1318d3e8de4c50f0d7d
droosif <ringodrew@gmail.com>
2022-02-17 10:54:21 -0500

    added safe list function

---
+++ R/safe_list.R
@@ -0,0 +1,28 @@
+safe_list <- function(.list) {
+  tryCatch({
+    obj <- as.list(.list)
+    obj <- lapply(obj, function(x){
+      if (is.character(x) && nchar(x) > 300) {
+        return(
+          paste0(
+            substr(x, 1, pmin(nchar(x), 300)),
+            "... [[ truncated for space ]]"
+          )
+        )
+      } else {
+        return(x)
+      }
+    })
+  }, error = function(e) {
+    message(e)
+    obj <- list(
+      "ERROR",
+      e,
+      "Please refresh the page to see if the error persists",
+      "If so, submit an issue here:",
+      "https://github.com/colearendt/shiny-session-info"
+    )
+  })
+  
+  return(obj)
+}
